755b5d6a33f8e181985facefbe9270f9103c648c
Or Brami <orbrami124@gmail.com>
2026-06-08 00:06:35 +0300

    Delete js directory

--- js/config.js
+++
@@ -1,6 +0,0 @@
-/**
- * GAYMOGGLE CONFIG
- * After deploying the backend to Render, paste your Render URL here.
- * Example: "https://gaymoggle-backend.onrender.com"
- */
-const BACKEND_URL = "https://gaymoggle.onrender.com";
